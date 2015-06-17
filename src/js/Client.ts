53693a0ceb748dbd13ac73fdcbeb1ac5d6126902
tolulope.bewaji <tolulope.bewaji@tricorde.net>
2015-06-17 10:45:53 +0100

    Updated Client to allow basic POSTs to server

--- src/js/Client.ts
+++ src/js/Client.ts
@@ -16,15 +16,43 @@ var client = {
 	    ({
 	        type: "POST",
 	        url: url,
+	        contentType : 'application/json',
 	        dataType: 'json',
 	        async: false,
 	        data: data,
 	        success: function () {
 	        	alert("Thanks!"); 
 	        },
-	        error: function(error_text) {
-	           alert("Update unsuccessful. Status: " + error_text);
-	        }	        
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert(xhr.status);
+                alert(xhr.responseText);
+            }        
 	    });                  	
     }
 };
+
+
+
+
+ // var message;
+ //            var statusErrorMap = {
+ //                '400' : "Server understood the request, but request content was invalid.",
+ //                '401' : "Unauthorized access.",
+ //                '403' : "Forbidden resource can't be accessed.",
+ //                '500' : "Internal server error.",
+ //                '503' : "Service unavailable."
+ //            };
+ //            if (x.status) {
+ //                message =statusErrorMap[x.status];
+ //                                if(!message){
+ //                                      message="Unknown Error \n.";
+ //                                  }
+ //            }else if(exception=='parsererror'){
+ //                message="Error.\nParsing JSON Request failed.";
+ //            }else if(exception=='timeout'){
+ //                message="Request Time out.";
+ //            }else if(exception=='abort'){
+ //                message="Request was aborted by the server";
+ //            }else {
+ //                message="Unknown Error \n.";
+ //            }
